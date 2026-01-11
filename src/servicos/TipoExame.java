53b08973f214e8bf54c1f009b4658a89ac3a1085
C-FK <fkalume@alu.ufc.br>
2026-01-11 13:24:56 -0300

    Update TipoExame.java

--- src/servicos/TipoExame.java
+++ src/servicos/TipoExame.java
@@ -1,5 +1,20 @@
 package servicos;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class TipoExame {
+    public static Map<String, Double> exames = new HashMap<>();
+
+    static {
+        exames.put("Teste Fiv/Felv", 150.00);
+        exames.put("Hemograma", 85.00);
+        exames.put("Ultrassom", 130.00);
+        exames.put("Raio-X", 120.00);
+        exames.put("Exame de Fezes", 74.00);
+        exames.put("Exame de Urina", 44.00);
+    }
+/*
 public class TipoExame {
     private String nome;
     private double precoBase;
@@ -16,4 +31,5 @@ public class TipoExame {
     public double getPrecoBase() {
         return precoBase;
     }
+    */
 }
