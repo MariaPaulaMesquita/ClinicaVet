920624b7c5c9c7df94932305784955fc44d34843
C-FK <fkalume@alu.ufc.br>
2026-01-11 13:51:56 -0300

    Update Vacinacao.java

--- src/servicos/Vacinacao.java
+++ src/servicos/Vacinacao.java
@@ -6,6 +6,28 @@ import java.util.HashMap;
 import java.time.LocalDateTime;
 
 public class Vacinacao extends Servico implements Preco{
+    private String nomeVacina;
 
+    Vacinacao(LocalDateTime dataHora, Animal animal, Veterinario veterinario, String nomeVacina){
+        super(dataHora, animal, veterinario);
+        this.nomeVacina = nomeVacina;
+        this.valorBase = TiposVacinas.vacinas.get(nomeVacina);
+    }
+
+    public double calcularPreco(Tutor tutor){
+        if(tutor.isMensalista()){
+            return this.valorBase*0.7; //desconto 30%
+        }else{
+            return this.valorBase;
+        }
+    }
+
+    public String getNomeVacina(){
+        return this.nomeVacina;
+    }
+
+    public String tipoServico(){
+        return "Vacina " + this.nomeVacina;
+    }
 
 }
