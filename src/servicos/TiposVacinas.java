7b8334c43bc4a3e75493aa4fded430b31018f32a
C-FK <fkalume@alu.ufc.br>
2026-01-11 13:18:24 -0300

    Update TiposVacinas.java

--- src/servicos/TiposVacinas.java
+++ src/servicos/TiposVacinas.java
@@ -1,8 +1,19 @@
 package servicos;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class TiposVacinas {
+    public static Map<String, Double> vacinas = new HashMap<>();
+
+    static {
+        vacinas.put("V5", 130.00);
+        vacinas.put("V10", 90.00);
+        vacinas.put("Raiva", 40.00);
+        vacinas.put("Gripe canina", 100.00);
+        vacinas.put("Giardia", 80.00);
+    }
+
 
 }
 
