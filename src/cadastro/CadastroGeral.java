3da12a045ea3b5ff6642eda01a8689a2c9317d4a
C-FK <fkalume@alu.ufc.br>
2026-01-13 15:20:35 -0300

    Update CadastroGeral.java

--- src/cadastro/CadastroGeral.java
+++ src/cadastro/CadastroGeral.java
@@ -8,11 +8,20 @@ import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
-public class CadastroGeral {
-    Set<Tutor> tutores = new TreeSet<>() ;
-    Set<Veterinario> veterinarios = new TreeSet<>();
-    Set<Servico> servicosAgendados = new TreeSet<>();
+public class CadastroGeral { //to na dúvida nos static
+    public static Set<Tutor> tutores = new TreeSet<>() ;
+    public static Set<Veterinario> veterinarios = new TreeSet<>();
+    public static Set<Servico> servicosAgendados = new TreeSet<>();
 
+    public static void cadastrarTutor(Tutor tutor){
+        tutores.add(tutor);
+    }
+     public static void cadastrarAnimal(Animal animal, Tutor tutor){
+        tutor.adicionarAnimal(animal);
+     }
 
+     public static void cadastrarVeterinario(Veterinario veterinario){
+        veterinarios.add(veterinario);
+     }
 
 }
